5276f5b63262b012a37df5a99ff77e2ef99abaa2
Ashmit mishra <ashmit.mishra20@gmail.com>
2026-08-08 23:05:52 +0530

    Time: 150 ms (37.04%), Space: 17.5 MB (60.12%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,34 +1,55 @@
 class Solution {
-private:
-    bool answer(int index, int target, vector<int>& nums,
-                vector<vector<int>>& dp) {
-        if (target == 0)
-            return true;
-        if (index == 0)
-            return (nums[0] == target);
-        if (dp[index][target] != -1)
-            return dp[index][target];
-        bool nottake = answer(index - 1, target, nums, dp);
-        bool take = false;
-        if (target >= nums[index]) {
-            take = answer(index - 1, target - nums[index], nums, dp);
-        }
-        return dp[index][target] = take | nottake;
-    }
-
+    // private:
+    //     bool answer(int index, int target, vector<int>& nums,
+    //                 vector<vector<int>>& dp) {
+    //         if (target == 0)
+    //             return true;
+    //         if (index == 0)
+    //             return (nums[0] == target);
+    //         if (dp[index][target] != -1)
+    //             return dp[index][target];
+    //         bool nottake = answer(index - 1, target, nums, dp);
+    //         bool take = false;
+    //         if (target >= nums[index]) {
+    //             take = answer(index - 1, target - nums[index], nums, dp);
+    //         }
+    //         return dp[index][target] = take | nottake;
 public:
     bool canPartition(vector<int>& nums) {
+
         int n = nums.size();
+
         int sum = 0;
-        for (int i = 0; i < n; i++) {
-            sum += nums[i];
-        }
-        if (sum % 2 != 0)
+        for (int x : nums)
+            sum += x;
+
+        if (sum % 2)
             return false;
+
         int target = sum / 2;
 
-        vector<vector<int>> dp(n, vector<int>(target + 1, -1));
+        vector<vector<bool>> dp(n, vector<bool>(target + 1, false));
 
-        return answer(n - 1, target, nums, dp);
+        for (int i = 0; i < n; i++)
+            dp[i][0] = true;
+
+        if (nums[0] <= target)
+            dp[0][nums[0]] = true;
+
+        for (int index = 1; index < n; index++) {
+
+            for (int j = 1; j <= target; j++) {
+
+                bool nottake = dp[index - 1][j];
+
+                bool take = false;
+                if (j >= nums[index])
+                    take = dp[index - 1][j - nums[index]];
+
+                dp[index][j] = take || nottake;
+            }
+        }
+
+        return dp[n - 1][target];
     }
 };
