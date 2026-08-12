1b77ccc6c3273b4c25d59f4e16b4247f01b75608
Ashmit mishra <ashmit.mishra20@gmail.com>
2026-08-12 19:03:11 +0530

    Time: 3 ms (85.58%), Space: 15.4 MB (55.12%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,17 +1,18 @@
 class Solution {
 private:
-    int answer(int ind, int target, vector<int>& nums){
+    int answer(int ind, int target, vector<int>& nums , vector<vector<int>>& dp){
         if(ind == 0){
             if(target == 0 && nums[0] == 0) return 2; // both +0 and -0 count separately
             if(target == 0 || target == nums[0]) return 1;
             return 0;
         }
-        int nottake = answer(ind-1, target, nums);
+        if(dp[ind][target]!= -1) return dp[ind][target];
+        int nottake = answer(ind-1, target, nums , dp);
         int take = 0;
         if(nums[ind] <= target){
-            take = answer(ind-1, target - nums[ind], nums);
+            take = answer(ind-1, target - nums[ind], nums , dp );
         }
-        return take + nottake;
+        return dp[ind][target] = take + nottake;
     }
 public:
     int findTargetSumWays(vector<int>& nums, int target) {
@@ -19,6 +20,7 @@ public:
         if(abs(target) > totalSum) return 0;
         if((totalSum + target) % 2 != 0) return 0;
         int P = (totalSum + target) / 2;
-        return answer(nums.size()-1, P, nums);
+        vector<vector<int>> dp(nums.size() , vector<int>(P+1 , -1));
+        return answer(nums.size()-1, P, nums , dp);
     }
 };
