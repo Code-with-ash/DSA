c14b95c65f7676f13f72148d6db5db22a200e7f2
Ashmit mishra <ashmit.mishra20@gmail.com>
2026-08-12 23:05:09 +0530

    Time: 29 ms (54.55%), Space: 36.7 MB (49.64%) - LeetHub

---
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -0,0 +1,23 @@
+class Solution {
+    private:
+    int f(int ind , int target ,vector<int>& coins , vector<vector<int>> & dp){
+        if(ind == 0 ){
+            if(target == 0 ) return 1 ;
+            if(target%coins[0] ==0 ) return 1 ;
+            else return 0 ;
+        }
+        if(dp[ind][target]!=-1) return dp[ind][target];
+        int nottake = f(ind -1 , target , coins , dp);
+        int take = 0 ;
+        if(coins[ind]<=target){
+            take = f(ind , target- coins[ind] , coins , dp);
+        }
+        return dp[ind][target] = take + nottake ;
+    }
+public:
+    int change(int amount, vector<int>& coins) {
+        int n = coins.size();
+        vector<vector<int>> dp(n , vector<int>(amount+1 , -1));
+        return f(n-1 , amount , coins , dp);
+    }
+};
