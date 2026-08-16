8c1d69d131d72258a76ce34c6b68d02bc55a9f14
Ashmit mishra <ashmit.mishra20@gmail.com>
2026-08-16 22:58:55 +0530

    Time: 50 ms (55.9%), Space: 15.7 MB (73.61%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -1,35 +1,64 @@
 class Solution {
-private:
-    bool f(int i, int j, string& s, string& p, vector<vector<int>>& dp) {
-        // base case
-        if (i < 0 && j < 0)
-            return true;
-        if (j < 0 && i >= 0)
-            return false;
-        if (i < 0 && j >= 0) {
-            for (int ind = 0; ind <= j; ind++) {
-                if (p[ind] != '*')
-                    return false;
-            }
-            return true;
-        }
-        if (dp[i][j] != -1)
-            return dp[i][j];
-        if (s[i] == p[j] || p[j] == '?') {
-            return dp[i][j] = f(i - 1, j - 1, s, p, dp);
-        } else if (p[j] == '*') {
-            bool stay = f(i - 1, j, s, p, dp);
-            bool eliminate = f(i, j - 1, s, p, dp);
-            return dp[i][j] = eliminate || stay;
-        }
-        return dp[i][j] = false ;
-    }
+    // private:
+    //     bool f(int i, int j, string& s, string& p, vector<vector<int>>& dp) {
+    //         // base case
+    //         if (i < 0 && j < 0)
+    //             return true;
+    //         if (j < 0 && i >= 0)
+    //             return false;
+    //         if (i < 0 && j >= 0) {
+    //             for (int ind = 0; ind <= j; ind++) {
+    //                 if (p[ind] != '*')
+    //                     return false;
+    //             }
+    //             return true;
+    //         }
+    //         if (dp[i][j] != -1)
+    //             return dp[i][j];
+    //         if (s[i] == p[j] || p[j] == '?') {
+    //             return dp[i][j] = f(i - 1, j - 1, s, p, dp);
+    //         } else if (p[j] == '*') {
+    //             bool stay = f(i - 1, j, s, p, dp);
+    //             bool eliminate = f(i, j - 1, s, p, dp);
+    //             return dp[i][j] = eliminate || stay;
+    //         }
+    //         return dp[i][j] = false ;
+    //     }
 
 public:
     bool isMatch(string s, string p) {
         int n = s.size();
         int m = p.size();
-        vector<vector<int>> dp(n + 1, vector<int>(m + 1, -1));
-        return f(n - 1, m - 1, s, p, dp);
+        vector<vector<bool>> dp(n + 1, vector<bool>(m + 1, 0));
+        dp[0][0] = true;
+        for (int i = 1; i <= n; i++)
+            dp[i][0] = false;
+        for (int i = 1; i <= m; i++)
+            dp[0][i] = dp[0][i - 1] && (p[i - 1] == '*');
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (s[i - 1] == p[j - 1] || p[j - 1] == '?') {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else if (p[j - 1] == '*') {
+                    dp[i][j] = dp[i - 1][j] || dp[i][j - 1];
+                } else {
+                    dp[i][j] = false;
+                }
+            }
+        }
+        return dp[n][m];
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (s[i - 1] == p[j - 1] || p[j - 1] == '?') {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else if (p[j] == '*') {
+                    dp[i][j] = dp[i - 1][j] || dp[i][j - 1];
+                } else {
+                    dp[i][j] = false;
+                }
+            }
+        }
+        return dp[n][m];
     }
 };
