5e3ca54577821789e1cf2499a8ac4ebf9a7cb69e
Ashmit mishra <ashmit.mishra20@gmail.com>
2026-08-19 20:57:00 +0530

    Time: 287 ms (30.95%), Space: 296.5 MB (22.07%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,26 +1,36 @@
 class Solution {
-    int f(int ind, int prevIndex, vector<int>& nums, vector<vector<int>>& dp , int n) {
-        if (ind == n)
-            return 0;
+    // int f(int ind, int prevIndex, vector<int>& nums, vector<vector<int>>& dp
+    // , int n) {
+    //     if (ind == n)
+    //         return 0;
 
-        if (dp[ind][prevIndex + 1] != -1)
-            return dp[ind][prevIndex + 1];
+    //     if (dp[ind][prevIndex + 1] != -1)
+    //         return dp[ind][prevIndex + 1];
 
-        int take = 0;
+    //     int take = 0;
 
-        if (prevIndex == -1 || nums[ind] > nums[prevIndex])
-            take = 1 + f(ind + 1, ind, nums, dp , n);
+    //     if (prevIndex == -1 || nums[ind] > nums[prevIndex])
+    //         take = 1 + f(ind + 1, ind, nums, dp , n);
 
-        int dontTake = f(ind + 1, prevIndex, nums, dp , n);
+    //     int dontTake = f(ind + 1, prevIndex, nums, dp , n);
 
-        return dp[ind][prevIndex + 1] = max(take, dontTake);
-    }
+    //     return dp[ind][prevIndex + 1] = max(take, dontTake);
+    // }
 
 public:
     int lengthOfLIS(vector<int>& nums) {
         int n = nums.size();
-        vector<vector<int>> dp(n, vector<int>(n + 1, -1));
-
-        return f(0, -1, nums, dp , n);
+        vector<vector<int>> dp(n+1 , vector<int>(n + 1, 0));
+        for (int ind = n - 1; ind>= 0; ind--) {
+            for (int prevind = ind-1; prevind >= -1; prevind--) {
+                int take = 0;
+                if ( prevind==-1 || nums[ind] > nums[prevind]) {
+                    take = 1 + dp[ind + 1][ind+1];
+                }
+                int nottake = dp[ind + 1][prevind+1];
+                dp[ind][prevind+1] = max(take , nottake) ;
+            }
+        }
+        return dp[0][0];
     }
 };
