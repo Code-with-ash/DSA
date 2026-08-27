65cb8b9fe83c3bce163519cb4daa323c2d998f0a
Ashmit mishra <ashmit.mishra20@gmail.com>
2026-08-27 10:49:07 +0530

    Time: 4 ms (42.42%), Space: 23.4 MB (47.51%) - LeetHub

---
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -0,0 +1,47 @@
+class Solution {
+    private :
+    bool f(int i , int j ,vector<vector<char>>& board){
+        unordered_set<int> st ;
+        for(int row = i ; row < i + 3 ; row ++){
+            for(int col = j ; col < j + 3 ; col++){
+                if(board[row][col]=='.') continue ;
+                if(st.find(board[row][col])!=st.end()) return false ;
+                st.insert(board[row][col]);
+            }
+        }
+        st.clear();
+        return true ;
+    }
+public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+        unordered_set<int> set ;
+        for(int i = 0 ; i < 9 ; i++){
+            for(int j = 0 ; j < 9 ; j++){
+                if(board[i][j]=='.') continue ;
+                if(set.find(board[i][j])!= set.end()){
+                    // it already exists 
+                    return false ;
+                }
+                set.insert(board[i][j]);
+            }
+            set.clear();
+        }
+        for(int i = 0 ; i < 9 ; i++){
+            for(int j = 0 ; j < 9 ; j++){
+                if(board[j][i]=='.') continue ;
+                if(set.find(board[j][i])!= set.end()){
+                    // it already exists 
+                    return false ;
+                }
+                set.insert(board[j][i]);
+            }
+            set.clear();
+        }
+        for(int i = 0 ; i < 9 ; i = i + 3 ){
+            for(int j = 0 ; j < 9 ; j = j + 3){
+            if(!f(i , j , board)) return false ;;
+            }
+        }
+        return true  ;
+    }
+};
