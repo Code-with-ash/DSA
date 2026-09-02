27627e2a305ec4d5b660ca4c26d0afe5d9f9ed45
Ashmit mishra <ashmit.mishra20@gmail.com>
2026-09-02 23:22:01 +0530

    Time: 0 ms (100%), Space: 19.4 MB (88.13%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -9,25 +9,53 @@
  * };
  */
 class Solution {
+    // if (temp1->val > temp2->val) {
+    //         head = temp2 ;
+    //         temp2 = temp2 ->next ;
+    //     } else {
+    //         head = temp1 ;
+    //         temp1 = temp1->next ;
+    //     }
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        if (list1 == nullptr && list2 == nullptr)
+            return nullptr;
+        if (list2 == nullptr)
+            return list1;
+        if (list1 == nullptr)
+            return list2;
         ListNode* temp1 = list1;
         ListNode* temp2 = list2;
-        ListNode* dummy = new ListNode(-1); // just to keep track of head ;
-        ListNode* curr = dummy ;
-        while(temp1 && temp2){
-            if(temp1->val < temp2->val){
-                curr->next = temp1 ;
-                curr = temp1 ;
-                temp1 = temp1->next ;
-            }else{
-                curr->next = temp2 ;
-                curr = temp2;
-                temp2 = temp2->next ;
+        ListNode* curr = nullptr;
+        ListNode* prev = nullptr;
+        ListNode* head = nullptr;
+
+        if (temp1->val < temp2->val) {
+            head = temp1;
+            prev = temp1;
+            temp1 = temp1->next;
+        } else {
+            head = temp2;
+            prev = temp2;
+            temp2 = temp2->next;
+        }
+        while (temp1 && temp2) {
+            if (temp1->val < temp2->val) {
+                curr = temp1->next;
+                prev->next = temp1;
+                prev = temp1;
+                temp1 = curr;
+            } else {
+                curr = temp2->next;
+                prev->next = temp2;
+                prev = temp2;
+                temp2 = curr;
             }
         }
-        if(temp1) curr->next = temp1;
-        else curr->next = temp2 ;
-        return dummy->next ;
+        if (temp1)
+            prev->next = temp1;
+        else
+            prev->next = temp2;
+        return head;
     }
 };
