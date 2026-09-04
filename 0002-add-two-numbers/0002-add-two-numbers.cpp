74bc4234d3ca4cfab884529b77fa0d2bfe320729
Ashmit mishra <ashmit.mishra20@gmail.com>
2026-09-04 13:08:09 +0530

    Time: 0 ms (100%), Space: 77.2 MB (45.97%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.cpp
+++ 0002-add-two-numbers/0002-add-two-numbers.cpp
@@ -11,28 +11,26 @@
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode* temp1 = l1;
-        ListNode* temp2 = l2;
-        ListNode* dummynode = new ListNode(-1);
-        ListNode* curr = dummynode; 
-        int sum = 0 ;
-        int carry = 0;
-        while(temp1 || temp2){
-            int sum = carry; 
-            if(temp1) sum+=temp1->val;
-            if(temp2) sum+=temp2->val;
-
-            ListNode* node = new ListNode(sum%10);
-            carry = sum / 10 ; 
-            curr->next = node;
-            curr = node ; 
-            if(temp1) temp1 = temp1->next;
-            if(temp2) temp2 = temp2->next;
+        ListNode* temp = new ListNode((l1->val + l2->val) % 10);
+        int carry = (l1->val + l2->val) / 10;
+        ListNode* t1 = l1->next;
+        ListNode* t2 = l2->next;
+        ListNode* head = temp;
+        while (t1 || t2) {
+        int sum = carry ;
+            if (t1) sum += t1->val;
+            if (t2) sum += t2->val;
+            carry = sum / 10;
+            ListNode* x = new ListNode(sum % 10);
+            temp->next = x;
+            temp = x;
+            if(t1) t1 = t1->next;
+            if(t2) t2 = t2->next;
         }
-        if (carry) {
-            ListNode* newNode = new ListNode(carry);
-            curr->next = newNode;
+        if (carry>0) {
+            ListNode* carries = new ListNode(carry);
+        temp->next = carries;
         }
-        return dummynode->next ;
+        return head;
     }
 };
