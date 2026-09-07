83a2db2138723f6114868f4722e85a805c5789ca
Ashmit mishra <ashmit.mishra20@gmail.com>
2026-09-07 22:57:58 +0530

    Time: 29 ms (12.79%), Space: 23.6 MB (35.67%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.cpp
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -9,17 +9,21 @@
  */
 
 class Solution {
+    private:
+    TreeNode* f(TreeNode* node , TreeNode* p , TreeNode* q){
+        if(node == nullptr ) return nullptr ;
+        if(node->val == p->val ) return node ;
+        if(node->val == q->val ) return node ;
+        TreeNode* left = f(node->left , p , q );
+        TreeNode* right = f(node->right , p , q );
+        if(left && right ) return node ;
+        else if(left) return left ;
+        else if(right)  return right ;
+        else return nullptr ;
+    }
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-    TreeNode* node = root;
-    while (node) {
-        if (node->val > p->val && node->val > q->val)
-            node = node->left;         // both on left
-        else if (node->val < p->val && node->val < q->val)
-            node = node->right;        // both on right
-        else
-            return node;               // split point = LCA
+        if(root->val == p->val || root->val == q->val ) return root ;
+        return f(root , p , q );
     }
-    return nullptr;
-}
 };
