221dad3681608d8e5bfd7c958aa0978908dd3f46
Ashmit mishra <ashmit.mishra20@gmail.com>
2026-09-11 22:55:46 +0530

    Time: 38 ms (49.24%), Space: 49.4 MB (31.26%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,50 +1,25 @@
 class Solution {
-    // private :
-    // int answer(int ind , int target , vector<int>& coins , vector<vector<int>>& dp){
-    //     if(ind == 0 ){ 
-    //         if(target%coins[ind]== 0 ){
-    //             // possible
-    //             return target / coins[ind] ; 
-    //         }else{
-    //             // never possible 
-    //             return 1e9;
-    //         }
-    //     }
-    //     if(dp[ind][target]!=-1) return dp[ind][target];
-    //     int nottake = 0 + answer(ind-1 , target , coins , dp);
-    //     int take = INT_MAX ;
-    //     if(coins[ind] <= target){
-    //         take = 1 + answer(ind , target - coins[ind] , coins , dp );
-    //     }
-    //   return dp[ind][target] = min(take , nottake);
-    // }
+    int f(int ind, vector<int>& coins, int target ,vector<vector<int>>& dp) {
+        if (ind == 0) {
+            if (target % coins[0] == 0)
+                return target / coins[0];
+            return 1e9;
+        }
+        if(dp[ind][target]!=-1) return dp[ind][target];
+        int dontPick = f(ind - 1, coins, target , dp);
+
+        int pick = 1e9;
+        if (coins[ind] <= target)
+            pick = 1 + f(ind, coins, target - coins[ind] , dp);
+
+        return dp[ind][target] = min(pick, dontPick);
+    }
 
 public:
     int coinChange(vector<int>& coins, int amount) {
         int n = coins.size();
-        vector<vector<int>> dp(n , vector<int>(amount + 1 , -1 ));
-        for(int i = 0 ; i <= amount ; i++){
-             if( i % coins[0] == 0 ){
-                // possible
-                dp[0][i] = i / coins[0] ; 
-            }else{
-                // never possible 
-                dp[0][i] = 1e9;
-            }
-        }
-
-        for(int ind = 1 ; ind < n ; ind++ ){
-            for(int target = 0 ; target <= amount ; target++){
-                int nottake = dp[ind-1][target];
-                int take = INT_MAX ;
-                if(coins[ind]<= target){
-                    take = 1 + dp[ind][target - coins[ind]];
-                }
-                dp[ind][target] = min(take , nottake);
-            }
-        }
-        int ans = dp[n-1][amount];
-        if(ans == 1e9 ) return -1 ;
-        else return ans ;
+        vector<vector<int>> dp(n, vector<int>(amount + 1, -1));
+        int ans = f(coins.size() - 1, coins, amount , dp);
+        return ans >= 1e9 ? -1 : ans;
     }
 };
